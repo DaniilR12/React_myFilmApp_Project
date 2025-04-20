73bb5d8312f39de2ebcc7b10fc6b7fcca897d79d
Daniil Riazanov <ryazanow45@gmail.com>
2025-04-20 13:28:42 +0200

    Filter Favorite

--- src/components/Movies/FavoriteMoviesList.jsx
+++ src/components/Movies/FavoriteMoviesList.jsx
@@ -4,15 +4,24 @@ import MoviesCard from "./MoviesCard";
 export default function FavoriteMovies() {
   const { favoritesMovies } = useSelector((store) => store.favoritesSlice);
 
+  const { filterMoviesCategories } = useSelector((store) => store.movies)
+
+  const favoritesId = favoritesMovies.map(movie=>movie.id)
+  const moviesToShow = filterMoviesCategories.length > 0 ?
+  filterMoviesCategories.filter(film=>favoritesId.includes(film.id)):favoritesMovies
+  console.log(filterMoviesCategories);
+  
+
   return (
     <>
-      {favoritesMovies.length > 0 ? (
-        favoritesMovies.map((value, index) => (
+      {moviesToShow && moviesToShow.length > 0 ? (
+        moviesToShow.map((value, index) => (
           <MoviesCard key={index} {...value} />
         ))
       ) : (
-        <p>You don`t have favorites Movie</p>
+        <p>You don't have favorite movies.</p>
       )}
     </>
   );
 }
+
